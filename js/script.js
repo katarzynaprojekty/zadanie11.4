c41b6120f35b8441d5860905dee6a94b39f6a887
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-05-20 17:19:51 +0200

    Addition to the task 11.4

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, ram, rom, batteryCapacity) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
-}
+    this.ram = ram;
+    this.rom = rom;
+    this.batteryCapacity = batteryCapacity;
+};
 
 Phone.prototype.printInfo = function () {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". ");
+};
+Phone.prototype.memoryInfo = function () {
+	console.log("RAM Size: " + this.ram + " (GB), ROM Size: " + this.rom +  "(GB).");
+};
+Phone.prototype.batteryInfo = function () {
+	console.log("Standard Battery Capacity: " + this.batteryCapacity + "(mAh).");
+};
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
+var iPhone6S = new Phone("Apple", 2250, "silver", 2, 16, 1715);
 iPhone6S.printInfo();
 
-var onePlusOne = new Phone("OnePlus", 1299, "black");
+var onePlusOne = new Phone("OnePlus", 1299, "black", 3, 16, 3100);
 onePlusOne.printInfo();
 
-var samsungGalaxyS6 = new Phone("Samsung", 1749, "white");
+var samsungGalaxyS6 = new Phone("Samsung", 1749, "white", 3, 32, 2550);
 samsungGalaxyS6.printInfo();
